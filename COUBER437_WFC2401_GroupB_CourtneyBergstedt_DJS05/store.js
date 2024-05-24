a22175c19cc58826134e08b729b5921268e7ba57
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-05-24 11:53:36 +0200

    fixed code on line 20

--- COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS05/store.js
+++ COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS05/store.js
@@ -1,23 +1,22 @@
 function createStore(reducer) {
     let state;
     let listeners = [];
-
-    //Function will get the current state
+  
+    // Function to get the current state
     function getState() {
-        return state;
+      return state;
     }
-
+  
     // Function to dispatch actions and update state
     function dispatch(action) {
-        state = reducer(state, action);
-        listeners.forEach(listener => listener());
+      state = reducer(state, action);
+      listeners.forEach(listener => listener());
     }
-
-    //Function will subscribe to state changes
+  
+    // Function to subscribe to state changes
     function subscribe(listener) {
-        listeners.push(listener);
-        return () => {
-            listeners = listeners.filter(1 => 1 !== listener);
-        };
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter(l => l !== listener);
+      };
     }
-}
