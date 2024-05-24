e52e88b2f4fb9f351ebe744c2a75daabfbeebc3a
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-05-24 12:29:37 +0200

    created function for counterReducer

--- COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS05/store.js
+++ COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS05/store.js
@@ -30,4 +30,18 @@ function createStore(reducer) {
     subscribe
   };
 }
+
+// Reducer function to handle actions and update state
+function counterReducer(state = { count: 0 }, action) {
+    switch (action.type) {
+        case 'ADD':
+            return { count: state.count + 1 };
+          case 'SUBTRACT':
+            return { count: state.count - 1 };
+          case 'RESET':
+            return { count: 0 };
+          default:
+            return state;
+    }
+}
  
